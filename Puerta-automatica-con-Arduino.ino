4f03d430b645e97dab25cbd992378cfa9e803dc0
Velasco <proyectoteos@gmail.com>
2018-11-30 10:56:52 -0600

    NEW: Diagram, code and project resources are available

---
+++ Puerta-automatica-con-Arduino.ino
@@ -0,0 +1,132 @@
+/*
+  Created by TEOS
+  Domotic with Arduino https://goo.gl/btZpjs
+  YouTube https://goo.gl/k7TUSZ
+  Instagram https://goo.gl/zEIjia
+  Facebook https://goo.gl/eivraR
+  Twitter https://goo.gl/du5Wgn
+  Github https://goo.gl/Xl5IiS
+  Google Plus https://goo.gl/gBnUdh
+*/
+
+#include<Ultrasonic.h> //https://github.com/JRodrigoTech/Ultrasonic-HC-SR04
+#include<Servo.h> //https://github.com/arduino-libraries/Servo
+
+//The TRIG and ECHO pins of the ultrasonic are defined and
+//servoRightT & servoLeftT objects are created to control the servos
+Ultrasonic ultrasonicT(6, 7);
+Servo servoRightT;
+Servo servoLeftT;
+
+//Assigning the pins to the LEDs
+const int ledOpenT = 2;
+const int ledClosedT = 4;
+
+//Variables to store positions of the servos, the distance of the ultrasonic, a flag that
+//allows to rectify the change of distance and starts a variable of minimum seconds to two
+int posRightT = 0;
+int posLeftT = 0;
+int distanceT = 0;
+int initialDistanceT = 0;
+bool flagT = false;
+int continuousSecondsT = 0;
+const int minimalSecondsT = 2;
+
+//loopTimeT and waitingDoorClosingT define the time (in milliseconds) that the events of
+//your function will last, timeElapsedT and timeElapsedDoorClosingT are variables
+//that will store the elapsed time
+const int loopTimeT = 200;
+unsigned long timeElapsedT = 0;
+const int waitingDoorClosingT = 1000;
+unsigned long timeElapsedDoorClosingT = 0;
+
+void setup() {
+  //The pins of the servos are defined
+  servoLeftT.attach(3);
+  servoRightT.attach(5);
+  //The servos, initially, will move 90 degrees
+  servoLeftT.write(90);
+  servoRightT.write(90);
+  pinMode(ledOpenT, OUTPUT);
+  pinMode(ledClosedT, OUTPUT);
+  digitalWrite(ledClosedT, HIGH);
+  //The variables invoke the millis() action
+  timeElapsedT = millis();
+  timeElapsedDoorClosingT = millis();
+}
+
+void loop() {
+  //A variable that calls the millis() function is created, then the function overflow is managed
+  unsigned long currentMillisLoopT = millis();
+  if ((unsigned long)(currentMillisLoopT - timeElapsedT) >= loopTimeT) {
+    //The distance is obtained in real time and stored in distanceT to be compared with
+    //the return value sumary(), if both distances are equal and the flag is true then
+    //the door is opened, otherwise, call the function beforeCloseDoor()
+    distanceT = ultrasonicT.Ranging(CM);
+    if (distanceT == summary()) {
+      if (distanceT < 10 && flagT == false)
+        openDoor();
+      else if (distanceT >= 10 && flagT == true)
+        beforeCloseDoor();
+    }
+    timeElapsedT = millis();
+  }
+}
+
+//This function returns the average of 4 readings of the distance, its purpose is to have
+//a more accurate data of the measurement
+int summary() {
+  int sumT = 0;
+  for (int iT = 0; iT < 3; iT ++) {
+    sumT = sumT + (distanceT = ultrasonicT.Ranging(CM));
+    delay(50);
+  }
+  initialDistanceT = sumT / 3;
+  return (initialDistanceT);
+}
+
+//openDoor() generates 2 events, one is in the servomotors to change its position
+//(both in opposite way) and another event changes the state of the LEDs
+void openDoor() {
+  flagT = true;
+  posLeftT = 90;
+  for (posRightT = 90; posRightT >= 0; posRightT -= 1) {
+    if (posLeftT <= 180) {
+      posLeftT++;
+      servoLeftT.write(posLeftT);
+    }
+    servoRightT.write(posRightT);
+    delay(15);
+  }
+  digitalWrite(ledOpenT, HIGH);
+  digitalWrite(ledClosedT, LOW);
+}
+
+//A timeout of +-3 seconds is granted and calls the closeDoor() function
+//You can modify the wait time in the variable minimalSecondsT
+void beforeCloseDoor() {
+  unsigned long currentMillisDoorT = millis();
+  if ((unsigned long)(currentMillisDoorT - timeElapsedDoorClosingT) >= waitingDoorClosingT ) {
+    continuousSecondsT++;
+    if (continuousSecondsT == minimalSecondsT)
+      closeDoor();
+    timeElapsedDoorClosingT = millis();
+  }
+}
+
+//This works in a manner contrary to the openDoor() function
+void closeDoor() {
+  flagT = false;
+  posLeftT = 180;
+  for (posRightT = 0; posRightT <= 90; posRightT += 1) {
+    if (posLeftT >= 90) {
+      posLeftT--;
+      servoLeftT.write(posLeftT);
+    }
+    servoRightT.write(posRightT);
+    delay(15);
+  }
+  continuousSecondsT = 0;
+  digitalWrite(ledClosedT, HIGH);
+  digitalWrite(ledOpenT, LOW);
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# Puerta automática con Arduino
+Este es un proyecto de una puerta automática con Arduino.
+
+### Empezando
+Este proyecto se basa en los sensores que hemos usado anteriormente, si no tienes mucha experiencia en el manejo de dichos sensores puedes consultar nuestros repositorios.
+
+- Sensor de distancia https://github.com/proyectoTEOS/Sensor-de-distancia-HC-SR04-con-Arduino
+- Servomotor https://github.com/proyectoTEOS/Servomotor-con-Arduino
+
+### Requisitos
+- Tener el IDE de [Arduino](https://www.arduino.cc/en/Main/Software) (o el de tu preferencia) con la versión más actual
+- Contar con las librerías [Ultrasonic](https://github.com/JRodrigoTech/Ultrasonic-HC-SR04) y [Servo](https://github.com/arduino-libraries/Servo)
+- Tener el material para hacer el proyecto(claro)
+
+### Materiales
+Enlistaremos los materiales que se necesitan para realizar el proyecto, pero puedes conseguir componentes similares.
+- 1 Arduino UNO R3
+- 2 Servomotores Futaba S3003
+- 1 Ultrasónico HC-SR04
+- 1 Led de color rojo y otro verde (o del color que prefieras)
+- 2 resistencias de 330ohms
+- 1 Protoboard (opcional)
+- **Fuente de poder de 5 volts a 1 Amper (indispensable para suministrar voltaje a los servos, por ningún motivo alimentes los servomotores con Arduino)**
+
+### Código
+**[Aqui](https://github.com/proyectoTEOS/Puerta-automatica-con-Arduino/blob/master/Puerta-automatica-con-Arduino.ino)** podrás obtener el link del código, esta comentado de tal forma que puedas entender su estructura y función.
+
+### Diagrama
+El siguiente esquemático muestra como se debe conectar todos los componentes con la placa.
+![](/resources/diagrama-puerta-automatica.jpg)
+
+Toma en cuenta estas consideraciones:
+- Conecta los servomotores a una fuente externa de voltaje (de 5 volts y 1 Amper como mínimo)
+- Puentear el GND de la fuente externa con el GND del Arduino
+- Si vas a utilizar otro tipo de servomotores, considera la carga que pueden soportar
+
+### Prototipo
+Aquí dejaremos imágenes de nuestro prototipo, pronto subiremos los pasos para que puedas realizar un prototipo similar.
+![](/resources/prototipo-puerta-automatica-1.jpg)
+![](/resources/prototipo-puerta-automatica-2.jpg)
